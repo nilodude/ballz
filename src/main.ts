2c89fd564074e87c39210dbd3f040f86c191f757
nilodude <alphone@protonmail.com>
2024-08-13 22:03:21 +0200

    need to fix glass shader. must be Principled BSDF

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,23 @@ import * as Loader from '../src/loader'
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
+let bola = new THREE.Group<THREE.Object3DEventMap>()
 let cacharro = new THREE.Group<THREE.Object3DEventMap>()
 let mango = new THREE.Group<THREE.Object3DEventMap>()
 
+bola = await Loader.loadModel(scene,'bola')
 cacharro = await Loader.loadModel(scene,'cacharro')
 mango  = await Loader.loadModel(scene,'mango')
+
 mango.position.y += 1.22293
+
+
+// 2.05839
 
 //CAMERA
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.x = 0.75
-camera.position.y = 2.1
+camera.position.y = 1.3
 camera.position.z = 2.3
 
 
@@ -47,11 +53,15 @@ scene.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img
 scene.backgroundBlurriness = 0
 
 //LIGHTS
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
-directionalLight.position.z += 1000;
-directionalLight.position.y += 100;
-scene.add( directionalLight );
-
+const light1 = new THREE.DirectionalLight( 0xfff9d8, 3 );
+light1.position.z += 1000;
+light1.position.y += 100;
+scene.add(light1);
+const light2 = new THREE.DirectionalLight( 0xfff9d8, 1 );
+light2.position.z -= 3000;
+light2.position.y += 220;
+light2.position.x += 2000;
+scene.add(light2);
 
 const gui = new GUI()
 
@@ -71,7 +81,7 @@ function animate() {
 
   delta = clock.getDelta()
   if(cacharro){
-    let pos = new THREE.Vector3(cacharro.position.x, cacharro.position.y +1, cacharro.position.z)
+    let pos = new THREE.Vector3(cacharro.position.x, cacharro.position.y +1.2, cacharro.position.z)
     camera.lookAt(pos)
   }
   if(mango){
