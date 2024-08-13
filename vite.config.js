398a9f5cc369b88a09e32155631ab838e24002f2
nilodude <alphone@protonmail.com>
2024-08-13 21:26:25 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
-  // build: {
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //       output: {
-  //           assetFileNames: ({name}) => {
-               
-  //               if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')){
-  //                   return 'assets/images/[name]-[hash][extname]';
-  //               }
-                
-  //               if (/\.(glb)$/.test(name ?? '')){
-  //                 return 'assets/models/[name]-[hash][extname]';
-  //               }
-            
-
-  //               return 'assets/[name]-[hash][extname]';  
-  //           }
-  //       },
-  //   }
-// },
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 })
