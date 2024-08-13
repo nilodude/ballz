d987e94a6b726656a08de15428cf2febb8bc0222
nilodude <alphone@protonmail.com>
2024-08-13 15:11:52 +0200

    basic boilerplate

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,42 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
+//SCENE
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
+//CAMERA
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
+//RENDERER
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+new OrbitControls(camera, renderer.domElement)
+
+//STATS (FPS)
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+//RESIZE EVENT LISTENER
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement)
+
+//WORLD OBJECTS
+scene.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+scene.backgroundBlurriness = 0
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
-
-const stats = new Stats()
-document.body.appendChild(stats.dom)
 
 const gui = new GUI()
 
@@ -42,11 +53,18 @@ const cameraFolder = gui.addFolder('Camera')
 cameraFolder.add(camera.position, 'z', 0, 20)
 cameraFolder.open()
 
+
+//ANIMATION LOOP
+const clock = new THREE.Clock()
+let delta
+
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  delta = clock.getDelta()
+
+  cube.rotation.x += delta
+  cube.rotation.y += delta
 
   renderer.render(scene, camera)
   stats.update()
